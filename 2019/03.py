07c7510f27c8334cb9a880e8d574b51086e07f90
Jonathan Skeate <skeate@gmail.com>
2019-12-10 09:04:15 -0500

    2019-03

---
+++ 2019/03.py
@@ -0,0 +1,32 @@
+from input03 import v
+from grid import Grid
+
+v = [x.split(',') for x in v.strip().split('\n')]
+
+steps = 0
+pn = 0
+def m(g,c):
+    global steps
+    global pn
+    steps += 1
+    if c in g:
+        if g[c][0] == pn: return g[c]
+        return (2, steps + g[c][1])
+    return (pn, steps)
+g = Grid(m)
+for pn, path in enumerate(v):
+    steps=0
+    g.loc = (0,0)
+    for step in path:
+        d = int(step[1:])
+        if step[0] == 'R': g.slide_x(d)
+        if step[0] == 'L': g.slide_x(-d)
+        if step[0] == 'U': g.slide_y(d)
+        if step[0] == 'D': g.slide_y(-d)
+
+intersections = { c: k for c,k in g.grid.items() if k[0] == 2 }
+ds = [sum(abs(c) for c in x) for x in intersections.keys()]
+print(min(ds))
+
+tss = [ts[1] for ts in intersections.values()]
+print(min(tss))
